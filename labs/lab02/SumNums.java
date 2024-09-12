ba0ebffa15a6692cf3494ebd0dfc0dee5afbf10a
Lovish Singh <singhlo9@dh2026pc01.utm.utoronto.ca>
2024-09-12 00:51:44 -0400

    SumUp completed

--- labs/lab02/SumNums.java
+++ labs/lab02/SumNums.java
@@ -12,12 +12,12 @@ public class SumNums {
 			if(args.length!=2){
 				System.out.println("Usage: java SumNums STARTNUM ENDNUM");
 				return;
-			}
+					}
 			int startNum = Integer.parseInt(args[0]);
 			int endNum = Integer.parseInt(args[1]);
-			int sum=0;
-			// You have something to fix here!
-			System.out.println("The sum from "+startNum+ " to "+ endNum + " is: "+ sum);
+			int sum = sumup(startNum , endNum);
+			//You have something to fix here!
+			{System.out.println("The sum from "+startNum+ " to "+ endNum + " is: "+ sum);}
 
 		} catch (NumberFormatException e){
 			System.out.println("One of the command line arguments is not an integer");
@@ -26,4 +26,16 @@ public class SumNums {
 
 	// Create static method sumup(s,e) which returns the sum s+(s+1)+...+(e-1)+e
 	// or 0 if this sum does not make sense (ie sumup(3,-4)).
+	public static int sumup(int s, int e) {
+		int current = s;
+		int sum = 0;
+		while (current != (e+1)) {
+			// System.out.println("sum" + sum + "current" + current + "s" + s + "e" + e);
+			sum += current;
+			current ++;
+			}
+		return sum;
+			}
+
+
 }
